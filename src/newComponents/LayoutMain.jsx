189b213c3fdae470707e13ae6d74c503179929e0
Abdulqudoos-dot <wirelesstechfsd@gmail.com>
2024-09-22 01:21:15 +0500

    Update LayoutMain.jsx

--- src/newComponents/LayoutMain.jsx
+++ src/newComponents/LayoutMain.jsx
@@ -4,15 +4,15 @@ import { Provider, useDispatch } from "react-redux";
 import { store } from "@/redux/store";
 import mixpanel from "mixpanel-browser";
 import Navbar from "./LayoutNav";
-import PrivateRoute from "@/navigation/PrivateRoute";
+// import PrivateRoute from "@/navigation/PrivateRoute";
 mixpanel.init("4fbbe7193eeaa9f4e91b347194243689");
 
 const LayoutMain = ({ children }) => {
   return (
     <Provider store={store}>
-      <PrivateRoute>
-        <Navbar>{children}</Navbar>
-      </PrivateRoute>
+      {/* <PrivateRoute> */}
+      <Navbar>{children}</Navbar>
+      {/* </PrivateRoute> */}
     </Provider>
   );
 };
